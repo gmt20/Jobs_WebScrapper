de9552e00567f962d9dddbb515e171ec92f3ca76
gmt20 <41137358+gmt20@users.noreply.github.com>
2019-10-22 14:11:19 +0530

    Update Jobs_WebScrapper.py

--- Jobs_WebScrapper.py
+++ Jobs_WebScrapper.py
@@ -38,17 +38,17 @@ for i in range(2):
         for container in res:
             #title
             title_class = container.select(".row .jobtitle")[0]
-            print(title_class)
+            #print(title_class)
             if title_class is not None and title_class != []:
-                link_class = title_class.find("a")
-                print(link_class)
-                if link_class is not None and link_class != []:
-                    title_desc = link_class.get("title")
-                    link_desc= "https://www.indeed.co.in"+ link_class.get("href")
-                    title_list.append(title_desc)
-                    link_list.append(link_desc)
-                    print(title_desc)
-                    print(link_desc)
+                #link_class = title_class.find("a")
+                #print(link_class)
+                #if link_class is not None and link_class != []:
+                title_desc = link_class.get("title")
+                link_desc= "https://www.indeed.co.in"+ link_class.get("href")
+                title_list.append(title_desc)
+                link_list.append(link_desc)
+                    #print(title_desc)
+                    #print(link_desc)
             else:
                 title_list.append("No Information available")
                 link_list.append("No Information available")
@@ -58,10 +58,10 @@ for i in range(2):
             company_class = container.select(".row .company")
                 
             if company_class is not None and company_class != []:
-                print(company_class)
+                #print(company_class)
                 company_desc = company_class[0].text.lstrip()
                 company_list.append(company_desc)
-                print(company_desc)
+                #print(company_desc)
             else:
                 company_list.append("No Information available")
             
@@ -69,10 +69,10 @@ for i in range(2):
             location_class = container.select(".row .location")
                 
             if location_class is not None and location_class != []:
-                print(location_class)
+                #print(location_class)
                 location_desc = location_class[0].text
                 loc_list.append(location_desc)
-                print(location_desc)
+                #print(location_desc)
             else:
                 loc_list.append("No Information available")
             
@@ -80,10 +80,10 @@ for i in range(2):
             salary_class = container.select(".row .salarySnippet .salary")
                 
             if salary_class is not None and salary_class != []:
-                print(salary_class)
+                #print(salary_class)
                 salary_desc = salary_class[0].text.lstrip()
                 salary_list.append(salary_desc)
-                print(salary_desc)
+                #print(salary_desc)
             else:
                 salary_list.append("No Information available")
         
@@ -93,22 +93,22 @@ for i in range(2):
             summary_class = container.select(".row .snip .summary")
                 
             if summary_class is not None and summary_class != []:
-                print(summary_class)
+                #print(summary_class)
                 summary_desc = summary_class[0].text.lstrip()
                 summary_list.append(summary_desc)
-                print(summary_desc)
+                #print(summary_desc)
             else:
                 summary_list.append("No Information available")
     time.sleep(10)
     page_no += 10           
-print(company_list)
-            
-print( len(title_list))
-print( len (link_list))
-print( len(company_list))
-print(len(loc_list))
-print(len(salary_list))
-print(len(summary_list))
+#print(company_list)
+# 
+#print( len(title_list))
+#print( len (link_list))
+#print( len(company_list))
+#print(len(loc_list))
+#print(len(salary_list))
+#print(len(summary_list))
 cols = ['Title', 'Company', 'Location', 'Link', 'Salary', 'Summary']
 jobs = pd.DataFrame({'Title': title_list,
                            'Company': company_list,
